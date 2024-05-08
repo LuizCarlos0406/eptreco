65a68e80b8f3ced353459756d450c0626580da03
LuizCarlos0406 <161314972+LuizCarlos0406@users.noreply.github.com>
2024-05-08 09:54:09 -0300

    Add files via upload

---
+++ SERIES.js
@@ -0,0 +1,17 @@
+// Selecionando os botões pelo ID
+var botao1 = document.getElementById('botao1');
+var botao2 = document.getElementById('botao2');
+var botao3 = document.getElementById('botao3');
+
+// Adicionando event listeners para cada botão
+botao1.addEventListener('click', function() {
+    alert('Você clicou no Botão 1!');
+});
+
+botao2.addEventListener('click', function() {
+    alert('Você clicou no Botão 2!');
+});
+
+botao3.addEventListener('click', function() {
+    alert('Você clicou no Botão 3!');
+});
